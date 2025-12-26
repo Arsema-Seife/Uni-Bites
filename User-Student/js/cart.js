b8bec3e96432cd7be8ce60eefc044d4d481527de
BarkiZed <2118kots@gmail.com>
2025-12-26 10:04:26 +0300

    little update on cart

--- User-Student/js/cart.js
+++ User-Student/js/cart.js
@@ -20,6 +20,13 @@ class ShoppingCart {
     // Browse menu button
     const browseBtn = document.querySelector(".browse-btn");
     if (browseBtn) {
+      browseBtn.addEventListener("mouseover", () => {
+        browseBtn.style.transform = "scale(1.05)";
+      });
+
+      browseBtn.addEventListener("mouseout", () => {
+        browseBtn.style.transform = "scale(1)";
+      });
       browseBtn.addEventListener("click", () => {
         window.location.href = "menu.html";
       });
